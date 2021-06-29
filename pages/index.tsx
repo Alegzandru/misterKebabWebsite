24d1c706b1fb3833518e164ebda8ece7d49b83b9
Aurel <dodon.aurel@gmail.com>
2021-06-29 15:43:27 +0300

    back to top button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,17 +1,11 @@
-import { people } from '../src/constants'
+import React from 'react'
+
+import BackToTopButton from '../src/components/BackToTopButton/BackToTopButton'
 
 const MainPage = () => (
-  <ul className="divide-y divide-gray-200">
-    {people.map((person) => (
-      <li key={person.email} className="py-4 flex">
-        <img className="h-10 w-10 rounded-full" src={person.image} alt="" />
-        <div className="ml-3">
-          <p className="text-sm font-medium text-gray-900">{person.name}</p>
-          <p className="text-sm text-gray-500">{person.email}</p>
-        </div>
-      </li>
-    ))}
-  </ul>
+  <>
+    <BackToTopButton />
+  </>
 )
 
 export default MainPage
---
+++ src/components/BackToTopButton/BackToTopButton.tsx
@@ -0,0 +1,20 @@
+import classNames from 'classnames'
+import styles from './BackToTopButton.module.scss'
+
+const BackToTopButton = () => {
+  const arrow = (className: string) => (
+    <svg className={className} width="20" height="14" viewBox="0 0 16 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M14.25 8.125L8 1.875L1.75 8.125" stroke="#cacaca" strokeWidth="2.5" strokeLinecap="round" strokeLinejoin="round" />
+    </svg>
+  )
+
+  return (
+    <a href="#" className={classNames(styles.backToTopContainer, 'overflow-hidden rounded-full fixed flex justify-center items-center')}>
+      <div className={classNames(styles.backToTopContainer__innerCircle, 'rounded-full z-10')} />
+      {arrow(classNames(styles.backToTopContainer__arrowUp, 'absolute z-20'))}
+      {arrow(classNames(styles.backToTopContainer__arrowDown, 'absolute z-20'))}
+    </a>
+  )
+}
+
+export default BackToTopButton
