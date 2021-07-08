abad7e72bcf5a23b138c14559cadf4c3f5af68f1
Aurel <dodon.aurel@gmail.com>
2021-07-09 01:29:18 +0300

    parallax

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,14 +5,15 @@ import 'tailwindcss/tailwind.css'
 import { AppProps } from 'next/app'
 import Head from 'next/head'
 import React from 'react'
+import { ParallaxProvider } from 'react-scroll-parallax'
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
-  <>
+  <ParallaxProvider>
     <Head>
       <title>Mister Kebab</title>
     </Head>
     <Component {...pageProps} />
-  </>
+  </ParallaxProvider>
 )
 
 export default MyApp
