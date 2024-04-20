2b4165cad064573af13c9bd896de6fe24364a277
Alexandru Codreanu <alexandru.codreanu.04@gmail.com>
2024-04-20 15:51:10 +0300

    added glovo button

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -2,25 +2,19 @@ import classNames from 'classnames'
 import { useTranslation } from 'next-i18next'
 import Image from 'next/image'
 import { useRouter } from 'next/router'
-import React, { memo, useContext, useState } from 'react'
+import React, { memo, useState } from 'react'
 
 import { DRINKS, WEIGHT_TYPE } from '../../constants'
 import { LANGUAGES, SIZES } from '../../constants/common'
-import { CartContext } from '../../store/Cart/Cart.context'
-import { ModalContext } from '../../store/Modal/Modal.context'
 import { Product } from '../../types'
 import Badges from '../Badges/Badges'
 import ProductCount from '../ProductCount/ProductCount'
-import Bag from '../Svgs/Bag/Bag'
 import styles from './ProductCard.module.scss'
 
 type Props = Product
 
 const ProductCard = memo((props: Props) => {
   const { name, nameru, price, image, weight, badges, subcategory } = props
-
-  const { actions: { showProductModal } } = useContext(ModalContext)
-  const { actions: { addProducts } } = useContext(CartContext)
 
   const [count, setCount] = useState(1)
 
@@ -29,18 +23,11 @@ const ProductCard = memo((props: Props) => {
   const router = useRouter()
   const isRo = router.locale === LANGUAGES.ro
 
-  const addToCartHandler = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-    event.stopPropagation()
-
-    addProducts(name, nameru, count)
-    setCount(1)
-  }
 
   return (
     <div
       role="button"
       className={classNames(styles.productCardContainer, 'w-full relative rounded flex flex-col bg-white font-bold transition-all')}
-      onClick={() => showProductModal(props)}
     >
       <div className={classNames(styles.productCardContainer__imageContainer, 'relative w-full flex-1 transition-all duration-500')}>
         <Image className="rounded-t" src={image} alt={name} layout="fill" objectFit="cover" quality={80} />
@@ -71,16 +58,17 @@ const ProductCard = memo((props: Props) => {
           'flex w-full mt-auto lg:absolute lg:opacity-0 transition-all duration-500')
         }>
           <ProductCount className="hidden lg:flex" background="white" size={SIZES.md} value={count} onChange={setCount} />
-          <button
+          <a
             className={classNames(
               styles.productCardContainer__addToCartButton,
               'flex w-full justify-center items-center font-bold transition-colors duration-300'
             )}
-            onClick={addToCartHandler}
+            href={isRo ? 'https://glovoapp.com/md/ro/chisinau/mrkebab/' : 'https://glovoapp.com/md/ru/kishinyov/mrkebab/'}
+            target="_blank"
+            rel="noreferrer"
           >
-            {t('în coș')}
-            <Bag className={classNames(styles.productCardContainer__bag, 'ml-2 transition-all duration-300')} />
-          </button>
+            {t('Comandă pe Glovo')}
+          </a>
         </div>
       </div>
     </div>
